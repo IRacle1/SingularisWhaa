d0c52e953e80abf8a27725a8ba2bbb73cbda78bd
IRacle <zornik2006@yandex.ru>
2025-04-14 21:55:14 +0300

    Добавьте файлы проекта.

---
+++ SingularisWhaa/Program.cs
@@ -0,0 +1,28 @@
+namespace SingularisWhaa
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllers();
+
+            var app = builder.Build();
+
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
---
+++ SingularisWhaa/Services/ISigmaService.cs
@@ -0,0 +1,15 @@
+﻿using Hangfire;
+using Hangfire.Server;
+
+namespace SingularisWhaa.Services
+{
+    public class CleanTempDirectoryProcess : BackgroundService
+    {
+        public void Execute(BackgroundProcessContext context)
+        {
+            BackgroundJob.Enqueue()
+            Directory.CleanUp(Directory.GetTempDirectory());
+            context.Wait(TimeSpan.FromHours(1));
+        }
+    }
+}
